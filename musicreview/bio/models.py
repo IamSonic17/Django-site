94f520c576d15e0057892a18d6fdf49feefe640a
IamSonic17 <ilyaborisov@inbox.ru>
2023-11-26 20:12:08 +0500

    Add model. Update urls and views.

--- musicreview/bio/models.py
+++ musicreview/bio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Bio(models.Model):
+    title = models.CharField('Название', max_length=50)
+    description = models.CharField('Описание', max_length=250)
+    full_text = models.TextField('Текст', max_length=5500)
+    date = models.DateField('Дата Публикации')
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Биография"
+        verbose_name_plural = "Биографии"
--- musicreview/bio/views.py
+++ musicreview/bio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Bio
 
 
 def bio_home(request):
-    return render(request, template_name='bio/bio_home.html')
+    bios = Bio.objects.order_by('date')
+    return render(request, template_name='bio/bio_home.html', context={'bios': bios})
+
